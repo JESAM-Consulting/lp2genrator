03d9103b6c6c00a6594664557cfc95bd339ebeb2
kevin <kevin088.rejoice@gmail.com>
2023-01-31 16:54:57 +0530

    Fix design issue in stepper

--- src/components/steper/steperLine/index.js
+++ src/components/steper/steperLine/index.js
@@ -22,7 +22,9 @@ export default function SteperLine(props) {
                             }
                               
                         </div>
-                        <p>Über dich</p>    
+                        {tabview === "first" || tabview === "second" || tabview === "third" || tabview ===  "forth" ?
+                        <p  style={{color:"#9DBC78"}}>Über dich</p>    
+                        : <p>Über dich</p> }
                     </div>    
                     <div className="steper-icon-text-design">
                         <div className="icon-center-alignment">
@@ -40,7 +42,11 @@ export default function SteperLine(props) {
                             <div className="icon-design">
                             </div> }
                         </div>
-                        <p>Kontakt</p>    
+
+
+                        { tabview === "second" || tabview === "third" || tabview ===  "forth" ?
+                        <p  style={{color:"#9DBC78"}}>Kontakt</p>    
+                        : <p>Kontakt</p> }   
                     </div>    
                     <div className="steper-icon-text-design">
                         <div className="icon-center-alignment">
@@ -51,7 +57,10 @@ export default function SteperLine(props) {
                             <div className="icon-design">
                             </div>    }
                         </div>
-                        <p>FE Finance</p>    
+
+                        {tabview === "third" || tabview ===  "forth" ?
+                        <p  style={{color:"#9DBC78"}}>FE Finance</p>    
+                        : <p>FE Finance</p> }  
                     </div>    
                 </div>    
             </div>            
